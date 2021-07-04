69790f780ed5cd40f3c9bf457537e2d04dc339d7
katsu-piko <katsudon0064@gmail.com>
2021-07-04 23:28:03 +0900

    Redux実装

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
+import reducer from './redux/reducers'
 
-ReactDOM.render(<App />,document.getElementById('root'));
+const Store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={Store}>
+    <App />
+  </Provider>,
+document.getElementById('root'));
---
+++ src/redux/actions/types.js
@@ -0,0 +1,16 @@
+// 「数字」がクリックされたとき
+export const INPUT_NUMBER = 'INPUT_NUMBER';
+
+// 演算子がクリックされたとき
+export const PLUS = 'PLUS';
+export const MINUS = 'MINUS';
+export const MULTIPLY = 'MULTIPLY';
+export const DIVIDE = 'DIVIDE';
+
+// 「=」がクリックされたとき
+export const EQUAL = 'EQUAL';
+
+// 「AC」がクリックされたとき
+export const CLEAR = 'CLEAR';
+
+
